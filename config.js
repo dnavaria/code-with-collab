c45f54d81030c28063d45cfb02ef09ad9a9b2c65
Deepak Navaria <dk17816@gmail.com>
2020-09-23 11:29:43 +0530

    codewithcollab

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
       user: 'YOUR_GMAIL_ADDRESS',
       pass: 'YOUR_GMAIL_PASSWORD'
     }
+  dbConnstring: '',
   },
-  dbConnstring: 'mongodb://admin:123@ds137760.mlab.com:37760/code4share',
-  sessionKey: 'HaloCode4Share'
+  sessionKey: ''
 }
